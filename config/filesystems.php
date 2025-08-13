ecbc7ee47c16cdf64aa19c2274090885cfa9a303
uitsommsu <uitso@mmsu.edu.ph>
2025-08-13 15:36:43 +0800

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -36,23 +36,28 @@ return [
             'throw' => false,
         ],
 
+        // Modified "public" disk to use Cloudflare R2
         'public' => [
-            'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'driver' => 's3',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION', 'auto'),
+            'bucket' => env('AWS_BUCKET'),
+            'endpoint' => env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => true,
             'visibility' => 'public',
             'throw' => false,
         ],
 
+        // S3 disk also points to Cloudflare R2
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
+            'region' => env('AWS_DEFAULT_REGION', 'auto'),
             'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'use_path_style_endpoint' => true,
             'throw' => false,
         ],
 
